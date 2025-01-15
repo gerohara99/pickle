f45c14d1bb53b5e114300aaef5b267f4b349b65f
Ger O'Hara <gerohara99@gmail.com>
2025-01-15 21:22:20 +0000

    Have round robin and standouts working git add . Now need to output to schedule object

--- public/js/pickleScheduler.js
+++ public/js/pickleScheduler.js
@@ -1,61 +1,86 @@
-const aPlayers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-const numAcourts = 1;
-const capacityAcourts = 8; // Number of players on an A court per round
-const eventDurationMins = 150;
-const gameDurationMins = 10;
-const numOfRounds = Math.floor(eventDurationMins / gameDurationMins);
-const aNumStandOuts = aPlayers.length - capacityAcourts;
-let standOutsPerRound = [];
+const { filter } = require("compression");
 
-// Calculate Standouts
-let roundsFilled = false;
-let i = 0;
-let startSlice = 0;
-let playersLeft;
-let nettPlayersPerRound = [];
+exports.const = generateStandOutsPubJs = (
+  playersList,
+  numOfRounds,
+  numStandOuts
+) => {
+  // Calculate Standouts
+  let roundsFilled = false;
+  let i = 0;
+  let startSlice = 0;
+  let playersLeft;
+  let standOutsPerRound = [];
 
-do {
-  playersLeft = aPlayers.length - startSlice;
+  do {
+    playersLeft = playersList.length - startSlice;
 
-  if (playersLeft <= aNumStandOuts) {
-    playersFromNext = aNumStandOuts - playersLeft;
-    standOutsPerRound[i] = [].concat(
-      aPlayers.slice(-playersLeft),
-      aPlayers.slice(0, playersFromNext)
-    );
-    startSlice = playersFromNext;
-  } else {
-    standOutsPerRound[i] = aPlayers.slice(
-      startSlice,
-      startSlice + aNumStandOuts
-    );
-    startSlice += aNumStandOuts;
-  }
-  if (i === numOfRounds) roundsFilled = true;
-  i++;
-} while (!roundsFilled);
-
-// Calculate round robin
-const DUMMY = -1;
-// returns an array of round representations (array of player pairs).
-// http://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
-
-const rs = []; // rs = round array
-n = aPlayers.length;
-
-if (n % 2 === 1) {
-  aPlayers.push(DUMMY); // so we can match algorithm for even numbers
-  n += 1;
-}
-for (let j = 0; j < n - 1; j += 1) {
-  rs[j] = []; // create inner match array for round j
-  for (let i = 0; i < n / 2; i += 1) {
-    const o = n - 1 - i;
-    if (aPlayers[i] !== DUMMY && aPlayers[o] !== DUMMY) {
-      rs[j].push([aPlayers[o], aPlayers[i]]);
+    if (playersLeft <= numStandOuts) {
+      playersFromNext = numStandOuts - playersLeft;
+      standOutsPerRound[i] = [].concat(
+        playersList.slice(-playersLeft),
+        playersList.slice(0, playersFromNext)
+      );
+      startSlice = playersFromNext;
+    } else {
+      standOutsPerRound[i] = playersList.slice(
+        startSlice,
+        startSlice + numStandOuts
+      );
+      startSlice += numStandOuts;
     }
+    i === numOfRounds - 1 ? (roundsFilled = true) : i++;
+  } while (!roundsFilled);
+  return standOutsPerRound;
+};
+
+exports.const = generateAvailablePairingsPubJs = (playersList) => {
+  // Declare variables
+  const DUMMY = -1;
+  let avaibalePairings = [];
+
+  if (!playersList) return -1;
+
+  if (playersList.length % 2 === 1) {
+    playersList.push(DUMMY); // so we can match algorithm for even numbers
   }
-  console.log("aPlayers Before: ", aPlayers);
-  aPlayers.splice(1, 0, aPlayers.pop()); // permutate for next round
-  console.log("aPlayers Afterword: ", aPlayers);
-}
+  for (let j = 0; j < playersList.length - 1; j += 1) {
+    for (let i = 0; i < playersList.length / 2; i += 1) {
+      const o = playersList.length - 1 - i;
+      if (playersList[i] !== DUMMY && playersList[o] !== DUMMY) {
+        avaibalePairings.push({
+          playerA: playersList[o],
+          playerB: playersList[i],
+          pairingUsed: false,
+        });
+      }
+    }
+    playersList.splice(1, 0, playersList.pop()); // permutate for next round
+  }
+  return avaibalePairings;
+};
+
+exports.const = generateSchedulePubJs = (
+  availablePairings,
+  standOuts,
+  numOfRounds
+) => {
+  for (let i = 0; i < standOuts.length - 1; i++) {
+    let schedule = availablePairings.filter(
+      (element) =>
+        !standOuts[i].includes(element.playerA) &&
+        !standOuts[i].includes(element.playerB) &&
+        element.pairingUsed === false
+    );
+    console.log(standOuts, schedule);
+  }
+};
+
+const playersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const numStandOuts = 3;
+const numOfRounds = 11;
+let standOuts = [];
+
+standOuts = generateStandOutsPubJs(playersList, numOfRounds, numStandOuts);
+availablePairings = generateAvailablePairingsPubJs(playersList);
+schedule = generateSchedulePubJs(availablePairings, standOuts, numOfRounds);
