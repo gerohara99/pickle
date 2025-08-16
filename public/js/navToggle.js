164547f53cf3ed336ae2b5314c3dd7631f24230e
Ger O'Hara <gerohara99@gmail.com>
2025-08-16 15:25:43 +0100

    tidied up some ui stuff

--- public/js/navToggle.js
+++ public/js/navToggle.js
@@ -1,19 +1,18 @@
 export function initMobileNavToggle() {
   const mobileNavToggle = document.getElementById("mobileNavToggle");
-  const mobileDrawer = document.querySelector("nav.mobile-drawer");
+  const mobileDrawer = document.querySelector(".mobile-drawer");
+  const iconMenu = document.querySelector(".icon-menu");
+  const iconClose = document.querySelector(".icon-close");
 
   if (mobileNavToggle && mobileDrawer) {
     mobileNavToggle.addEventListener("click", () => {
       mobileDrawer.classList.toggle("open");
-
-      const iconMenu = mobileNavToggle.querySelector(".icon-menu");
-      const iconClose = mobileNavToggle.querySelector(".icon-close");
-
+      // Toggle icons
       if (mobileDrawer.classList.contains("open")) {
         iconMenu.style.display = "none";
-        iconClose.style.display = "inline-block";
+        iconClose.style.display = "inline";
       } else {
-        iconMenu.style.display = "inline-block";
+        iconMenu.style.display = "inline";
         iconClose.style.display = "none";
       }
     });
